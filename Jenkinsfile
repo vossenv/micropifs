8e31021f3af67ea8f0e4e34a785f90041c7c8f96
Danimae Janssen <janssenda@gmail.com>
2018-09-30 21:03:29 -0500

    add jar stash

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,13 +6,22 @@
 
 pipeline {
     agent any
+    environment {
+        jar_file = 'microcam-1.0.jar'
+    }
     stages {
         stage('Stage 1') {
             steps {
                 echo 'Hello world!'
                 sh 'chmod +x gradlew'
                 sh './gradlew clean build'
+                dir("build/libs") {
+                    stash includes: jar_file, name: 'micropicam'
+                    archiveArtifacts artifacts: '**', fingerprint: true
+                }
             }
+
+
         }
     }
     post {
--- src/main/java/com/dm/microcam/MicroController.java
+++ src/main/java/com/dm/microcam/MicroController.java
@@ -15,41 +15,16 @@ import java.io.IOException;
 @Controller
 public class MicroController {
 
-    @RequestMapping(value = {"/lock"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/simple"}, method = RequestMethod.GET)
     public Object getFrame(){
-
-
         return "/static/test.jpg";
     }
 
-    @RequestMapping(value = "/sid", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public void getImage(HttpServletResponse response) throws IOException {
-
-        ClassPathResource imgFile = new ClassPathResource("/static/test.jpg");
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-    }
-
-
-    @RequestMapping(value = "/sidre", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage() throws IOException {
-
-        ClassPathResource imgFile = new ClassPathResource("/static/test.jpg");
-        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
-
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(bytes);
-    }
 
     @RequestMapping(value = "/fail", method = RequestMethod.GET, produces = MediaType.IMAGE_GIF_VALUE)
     public ResponseEntity<InputStreamResource> getImageIRS() throws Exception {
 
-
         ClassPathResource imgFile = new ClassPathResource("/static/frame.gif");
-       //byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
-
 
         while (true){
             byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
