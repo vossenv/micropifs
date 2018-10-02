21732fe0c954d425d8814fe4a4a253236dfd2744
Danimae Janssen <janssenda@gmail.com>
2018-10-02 07:25:04 -0500

    many typo

--- install-microservice.sh
+++ install-microservice.sh
@@ -24,10 +24,10 @@ done
 ssh ${username}@${host} bash -c "'
 sudo mkdir $dir $1 &> /dev/null
 sudo wget https://raw.githubusercontent.com/janssenda/microservice-picam/master/micropifs.service -O /etc/systemd/system/micropifs.service
-sudo wget https://raw.githubusercontent.com/janssenda/microservice-picam/master/startup.sh -O $dir/micropifs/startup.sh
+sudo wget https://raw.githubusercontent.com/janssenda/microservice-picam/master/startup.sh -O $dir/startup.sh
 sudo systemctl daemon-reload
 sudo systemctl enable micropifs.service
 '"
 
-ssh $username@t$host sudo service micropifs restart
-ssh $username@t$host sudo service micropifs status
+ssh $username@$host sudo service micropifs restart
+ssh $username@$host sudo service micropifs status
