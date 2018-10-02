6dd7adec44e28ee5df3f96150f676bc6e61528c9
Danimae Janssen <janssenda@gmail.com>
2018-10-01 22:33:40 -0500

    Test deploy with services

--- install-microservice.sh
+++ install-microservice.sh
@@ -3,16 +3,27 @@
 username="pi"
 target="192.168.50.80"
 
-ssh $username@$target sudo mkdir /home/pi/micropifs $1 &> /dev/null
-ssh $username@$target sudo wget https://github.com/janssenda/microservice-picam/blob/master/micropifs.service -O /etc/systemd/system/micropifs.service
-ssh $username@$target sudo wget https://github.com/janssenda/microservice-picam/blob/master/startup.sh -O /home/pi/micropifs/startup.sh
-ssh $username@$target sudo systemctl daemon-reload
-ssh $username@$target sudo systemctl enable micropifs.service
+while [[ $# -gt 0 ]]
+do
+key=$1
+case $key in
+    -t)
+        target=$2
+        shift # past argument
+        shift # past value
+        ;;
+    -u)
+        username=$2
+        shift # past argument
+        shift # past value
+        ;;
+esac
+done
 
-
-#ssh $username@$target bash -c "'
-#sudo mkdir /home/pi/micropifs $1 &> /dev/null
-#sudo wget https://github.com/janssenda/microservice-picam/blob/master/micropifs.service -O /etc/systemd/system/micropifs.service
-#sudo wget https://github.com/janssenda/microservice-picam/blob/master/startup.sh -O /home/pi/micropifs/startup.sh
-#sudo systemctl daemon-reload
-#sudo systemctl enable micropifs.service
+ssh ${username}@${target} bash -c "'
+sudo mkdir /home/pi/micropifs $1 &> /dev/null
+sudo wget https://raw.githubusercontent.com/janssenda/microservice-picam/master/micropifs.service -O /etc/systemd/system/micropifs.service
+sudo wget https://raw.githubusercontent.com/janssenda/microservice-picam/master/startup.sh -O /home/pi/micropifs/startup.sh
+sudo systemctl daemon-reload
+sudo systemctl enable micropifs.service
+'"
