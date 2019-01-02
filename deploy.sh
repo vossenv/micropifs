5f82446eec7b8cf0d67d854a9181ef11439129e0
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-01 18:32:14 -0600

    add removal script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ ipaddresses=(
     "192.168.50.230:linux:pi"
     "192.168.50.227:linux:pi"
     "192.168.50.78:linux:pi"
-    "192.168.50.66:windows:jenkins"
-    "192.168.50.139:windows:jenkins"
+#    "192.168.50.66:windows:jenkins"
+#    "192.168.50.139:windows:jenkins"
+#    "192.168.50.187:linux:carag"
 )
 
 ./gradlew clean assemble
@@ -15,11 +16,26 @@ service_name="micropifs"
 
 function deployBash() {
 
-    local dir="/home/pi/.$service_name"
+    local dir="/home/$2/.$service_name"
     local script="sudo\ /usr/bin/java\ -jar\ micropifs.jar"
     ssh $2@$1 "sudo rm -rf $dir; echo "Creating directory $dir";  sudo mkdir $dir; sudo chmod 777 $dir;"
     scp build/libs/micropifs.jar $2@$1:$dir
     ssh $2@$1 'bash -s' < ./install-microservice-bash.sh "$dir" "$script" "$service_name"
+
+}
+
+function removeBash(){
+
+    local dir="/home/$2/.$service_name"
+
+    ssh $2@$1 "echo "Stopping service $service_name";  sudo service $service_name stop; sudo systemctl disable $service_name.service"
+    ssh $2@$1 "echo "Removing directory $dir";  sudo rm -rf $dir;"
+    ssh $2@$1 "echo "Removing configuration";  sudo rm /etc/systemd/system/$service_name.service;"
+    ssh $2@$1 "echo "Reload services";  sudo systemctl daemon-reload;"
+    printf "\nFinished removal of $service_name\n"
+
+sudo
+
 }
 
 function deployWin() {
@@ -48,7 +64,8 @@ for i in ${ipaddresses[@]}; do
     if [ "$platform" == "windows" ]; then 
         deployWin $host $username
     else 
-        deployBash $host $username
+        #deployBash $host $username
+        removeBash $host $username
     fi
 
     printf "===== End deploy: $platform / $username / $host =====\n"
