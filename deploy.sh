aed263425b65ec70e677d1b3a99448c504148a50
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-01 22:30:47 -0600

    Handle null pointer

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,22 @@ ipaddresses=(
 #    "192.168.50.227:linux:pi"
 #    "192.168.50.78:linux:pi"
 #    "192.168.50.66:windows:jenkins"
-#    "192.168.50.139:windows:jenkins"
-    "192.168.50.187:linux:self"
+    "192.168.50.139:windows:jenkins"
+#    "192.168.50.187:linux:self"
 #    "192.168.50.187:linux:carag"
 )
 
-#./gradlew clean assemble
+./gradlew clean assemble
 
 service_name="micropifs"
 
 function deployBash() {
-
     if [ $2 == "self" ]; then
+
         printf "\nDeploying to self... \n"
         local script="sudo /usr/bin/java -jar micropifs.jar"
         local dir="/home/carag/.$service_name"
+
         sudo rm -rf $dir; echo "Creating directory $dir";  sudo mkdir $dir; sudo chmod 777 $dir;
         sudo sudo chmod +x install-microservice-bash.sh
         sudo cp build/libs/micropifs.jar $dir
@@ -32,53 +33,57 @@ function deployBash() {
         scp build/libs/micropifs.jar $2@$1:$dir
         ssh $2@$1 'bash -s' < ./install-microservice-bash.sh "$dir" "$script" "$service_name"
     fi
+}
 
+function check_remote(){
+    echo -e "Checking API status remotely... \n"
+    apiStatus="down"
+    count=0
+    while [ "$apiStatus" != "true" ]; do
+        if [ "$count" -gt 50 ]; then break; fi
+        apiStatus=$(curl http://$1:9001/status -s)
+        count=$((count + 1))
+        sleep 1
+        echo -n "$count "
+    done
+    echo -e "\n"
+    [ "$apiStatus" = "true" ] && echo "Api is running, install complete!" || echo "Service not running, attention needed"
 }
 
 function removeBash(){
-
     local dir="/home/$2/.$service_name"
     ssh $2@$1 "echo "Stopping service $service_name";  sudo service $service_name stop; sudo systemctl disable $service_name.service"
     ssh $2@$1 "echo "Removing directory $dir";  sudo rm -rf $dir;"
     ssh $2@$1 "echo "Removing configuration";  sudo rm /etc/systemd/system/$service_name.service;"
     ssh $2@$1 "echo "Reload services";  sudo systemctl daemon-reload;"
     printf "\nFinished removal of $service_name\n"
-
-
 }
 
 function deployWin() {
-
     printf "Stopping service.... \n"
     timeout 5 ssh $2@$1 Stop-Service -Name Micropifs
-
     printf "\nBegin file transfer... \n"
     scp build/libs/micropifs.jar $2@$1:"C:/Program\\ Files\\ (x86)/Micropifs/micropifs.jar"
-
     printf "\nStarting service.... \n"
-
-    timeout 10 ssh $2@$1 Start-Service -Name Micropifs
+    timeout 5 ssh $2@$1 Start-Service -Name Micropifs
+    check_remote $1
 }
 
 for i in ${ipaddresses[@]}; do
 
     IFS=':' tokens=(${i});
-
     host=${tokens[0]}
     platform=${tokens[1]}
     username=${tokens[2]}
     
     printf "\n===== Begin deploy: $platform / $username / $host =====\n\n"
-    
     if [ "$platform" == "windows" ]; then 
         deployWin $host $username
     else 
         deployBash $host $username
         #removeBash $host $username
     fi
-
     printf "===== End deploy: $platform / $username / $host =====\n"
-
 done
 
 
--- src/main/java/com/dm/micropifs/MicroController.java
+++ src/main/java/com/dm/micropifs/MicroController.java
@@ -2,7 +2,7 @@ package com.dm.micropifs;
 
 import com.dm.micropifs.fileio.DataStore;
 import com.dm.micropifs.model.PiImage;
-import com.dm.micropifs.util.Deque;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,14 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class MicroController {
 
     private final DataStore ds;
-    private static List<PiImage> store = new Deque<>(5);
-    private int count = 0;
 
     @Inject
     public MicroController(DataStore ds) {
@@ -34,28 +31,6 @@ public class MicroController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Object index() {
         return "/static/index.html";
-    }
-
-    @RequestMapping(value = "/next", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<Object> getNext() {
-        PiImage next = store.get(0);
-        return ResponseEntity
-                .ok()
-                .headers(next.getHeaders())
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(next.getImage());
-    }
-
-    @ResponseBody
-    @RequestMapping(value = {"/receive"}, method = RequestMethod.POST)
-    public String uploadFile(@RequestParam MultipartFile file, HttpServletRequest request) {
-        try {
-            store.add(new PiImage(request, file));
-            this.count += 1;
-            return "Success --> total count: " + this.count;
-        } catch (Exception e) {
-            return e.getMessage();
-        }
     }
 
     @ResponseBody
@@ -81,12 +56,23 @@ public class MicroController {
 
     @RequestMapping(value = "/cameras/{camID}/next", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Object> getNextCameraImage(@PathVariable("camID") String camID) {
-        PiImage next = ds.getNext(camID.toLowerCase());
-        return ResponseEntity
-                .ok()
-                .headers(next.getHeaders())
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(next.getImage());
+        try {
+            PiImage next = ds.getNext(camID.toLowerCase());
+            return ResponseEntity
+                    .ok()
+                    .headers(next.getHeaders())
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(next.getImage());
+        } catch (Exception e) {
+            String type = e.getClass().getCanonicalName();
+            String msg = type + ": " + (type.contains("NullPointer")
+                    ? "No content stored for camera named '"+ camID + "'..."
+                    : e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(msg);
+        }
     }
 }
 
